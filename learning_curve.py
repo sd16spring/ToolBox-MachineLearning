b5001da8c29d2a75f124d07c77584fb26831e098
apurvaraman <apurva.raman@students.olin.edu>
2016-03-13 23:27:42 -0400

    ML toolbox written and tested!

--- learning_curve.py
+++ learning_curve.py
@@ -12,12 +12,29 @@ num_trials = 10
 train_percentages = range(5,95,5)
 test_accuracies = numpy.zeros(len(train_percentages))
 
+
+
 # train a model with training percentages between 5 and 90 (see train_percentages) and evaluate
 # the resultant accuracy.
 # You should repeat each training percentage num_trials times to smooth out variability
 # for consistency with the previous example use model = LogisticRegression(C=10**-10) for your learner
 
-# TODO: your code here
+for i in range(len(train_percentages)):
+    accuracy = []
+
+    for j in range(num_trials):
+
+        X_train, X_test, y_train, y_test = train_test_split(data.data,
+                                                            data.target,
+                                                            train_size=train_percentages[i]/100.0
+                                                            )
+        model = LogisticRegression(C=15 ** -10)
+        model.fit(X_train, y_train)
+        score = model.score(X_test,y_test)
+
+        accuracy.append(score)
+
+    test_accuracies[i] = numpy.average(accuracy)
 
 fig = plt.figure()
 plt.plot(train_percentages, test_accuracies)
