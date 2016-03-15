3226d2a308a7c7eb8afe17840736161c9d06fd5b
Kevin Zhang <kzhang8850@yahoo.com>
2016-03-15 19:50:53 -0400

    I have finished Machine Learning ToolBox, 6/5

--- learning_curve.py
+++ learning_curve.py
@@ -1,5 +1,11 @@
 """ Exploring learning curves for classification of handwritten digits """
 
+"""
+Completed by Kevin Zhang
+
+Sofware Design Spring 2016
+"""
+
 import matplotlib.pyplot as plt
 import numpy
 from sklearn.datasets import *
@@ -8,7 +14,7 @@ from sklearn.linear_model import LogisticRegression
 
 data = load_digits()
 print data.DESCR
-num_trials = 10
+num_trials = 100
 train_percentages = range(5,95,5)
 test_accuracies = numpy.zeros(len(train_percentages))
 
@@ -17,7 +23,26 @@ test_accuracies = numpy.zeros(len(train_percentages))
 # You should repeat each training percentage num_trials times to smooth out variability
 # for consistency with the previous example use model = LogisticRegression(C=10**-10) for your learner
 
-# TODO: your code here
+
+
+for training_index in range(len(train_percentages)):
+
+	data = load_digits()
+	stablizing_value = 0;   #temp variable to hold a bunch of value for smoothing out variability
+
+	for i in range(num_trials):    #repeated the test for each train_size value 10 times for more stability and smoothness
+		X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size=train_percentages[training_index]/100.0)
+		model = LogisticRegression(C=10**-10)
+		model.fit(X_train, y_train)
+		print "Training with {}%".format(train_percentages[training_index])
+		print "Train accuracy %f" %model.score(X_train,y_train)
+		print "Test accuracy %f"%model.score(X_test,y_test)
+		print ''
+		stablizing_value +=model.score(X_test, y_test)
+
+	stablizing_value /= num_trials	  #take the average of all the values you accumulated
+	test_accuracies[training_index] = stablizing_value
+
 
 fig = plt.figure()
 plt.plot(train_percentages, test_accuracies)
