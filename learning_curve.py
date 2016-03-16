54cc245733638a1f7e512d2580c7859061c17404
arianaolson419 <ariana.olson@students.olin.edu>
2016-03-15 20:10:22 -0400

    submitting this toolbox

--- learning_curve.py
+++ learning_curve.py
@@ -7,8 +7,8 @@ from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 data = load_digits()
-print data.DESCR
-num_trials = 10
+# print data.DESCR
+num_trials = 20
 train_percentages = range(5,95,5)
 test_accuracies = numpy.zeros(len(train_percentages))
 
@@ -17,10 +17,31 @@ test_accuracies = numpy.zeros(len(train_percentages))
 # You should repeat each training percentage num_trials times to smooth out variability
 # for consistency with the previous example use model = LogisticRegression(C=10**-10) for your learner
 
-# TODO: your code here
+
+def train_test(percent):
+	'''partitions data into training and testing sets, using these groups to train and test the data,
+	and returns tesing accuracy
+	percent: percent of data partitioned for training'''
+	X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size = percent/100.0)
+	model = LogisticRegression(C=10**0)
+	model.fit(X_train, y_train)
+	return model.score(X_test,y_test)
+
+for i in enumerate(train_percentages):
+	t = 0
+	for j in range(num_trials):
+		t += train_test(i[1])/num_trials #averages accuracies for each percentage
+	test_accuracies[i[0]] = t
+
+
 
 fig = plt.figure()
+# for i in range(10):
+# 	subplot = fig.add_subplot(5,2,i+1)
+# 	subplot.matshow(numpy.reshape(data.data[i], \
+# 		(8,8)), cmap='gray')
 plt.plot(train_percentages, test_accuracies)
 plt.xlabel('Percentage of Data Used for Training')
 plt.ylabel('Accuracy on Test Set')
+plt.title(str(num_trials))
 plt.show()
