3b0ac3988b0fdc1275ad68273523d101e6f2ef40
kuannie1 <anne.ku4@gmail.com>
2017-12-24 14:22:43 -0800

    updated documentation and added cross_validation

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# ToolBox-MachineLearning
-Machine Learning Project Toolbox starter code
+# Mini Project - MachineLearning
 
-Full instructions at https://sites.google.com/site/sd16spring/home/project-toolbox/machine-learning
+learning_curve.py is a simple script that takes in grayscale, 8x8 pixel numbers from the built-in scikit-learn database and classifies them with Multinomial Logistic Regression (MLR). 
+
+
+MLR works by partitioning the digits into two sets: a training set and a testing set. The training set is used for inferring the relationship between the current 8x8 picture and the estimated number. The testing set evaluates the training set's performance and applies it to the untrained, testing set. Having two sets is essential for avoiding possible overfitting to the training set. 
+
+The learning_curve() function then takes in the MLR results and plots the resulting curve.
+
+The cross_validation() function allows us to test a variety of C values to see which one works best. In our particular example, we tested 5 values: 10^(-4), 10^(-2), 1, 10^2, and 10^4.
+
+Initial code from https://sites.google.com/site/sd16spring/home/project-toolbox/machine-learning
--- learning_curve.py
+++ learning_curve.py
@@ -5,24 +5,23 @@ import numpy
 from sklearn.datasets import *
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.grid_search import GridSearchCV
 
-def display_nums(data, ):
-	digits = load_digits()
-	print digits.DESCR
+def display_nums(digits):
 	fig = plt.figure()
 	for i in range(10):
 		subplot = fig.add_subplot(5,2,i+1)
 		subplot.matshow(numpy.reshape(digits.data[i],(8,8)),cmap='gray')
 	plt.show()
 
-def mlr(data, num_trials, train_percentages, c=10**-10):
+def mlr(digits, num_trials, train_percentages, c=10**-10):
 	test_accuracies = [] 
 	for percentage in train_percentages:
 		x = 0
 		new_list = []
 		while x < num_trials:
 			x += 1
-			X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size=percentage)
+			X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, train_size=percentage)
 			model = LogisticRegression(C=c)
 			model.fit(X_train, y_train)
 			new_list.append(model.score(X_test,y_test))
@@ -30,19 +29,32 @@ def mlr(data, num_trials, train_percentages, c=10**-10):
 		test_accuracies.append(average)
 	return test_accuracies
 
-def learningcurve(data, num_trials, train_percentages, c=10**-10):
-	test_accuracies = mlr(data, num_trials, train_percentages, c)
+def learning_curve(train_percentages, test_accuracies):
 	print train_percentages
 	print test_accuracies
 	fig = plt.figure()
 	plt.plot(train_percentages, test_accuracies)
 	plt.xlabel('Percentage of Data Used for Training')
 	plt.ylabel('Accuracy on Test Set')
-	plt.suptitle('Regression With c ='+str(c))
+	plt.suptitle('Regression Result')
 	plt.show()
 
+def cross_validation(digits):
+	tuned_parameters = [{'C': [10**-4, 10**-2, 10**0, 10**2, 10**4]}]
+	X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, train_size=.9)
+
+	model = GridSearchCV(LogisticRegression(), tuned_parameters, cv=5)
+	model.fit(X_train, y_train)
+
+	print "best estimator: \n", model.best_estimator_
+	print "score: ", model.score(X_test, y_test)
+
+
 if __name__ == "__main__":
-	data = load_digits()
+	# Load the digits dataset from scikit-learn
+	digits = load_digits()
+	# display_nums(digits)
 	num_trials = 50
 	train_percentages = range(5,95,5)
-	learningcurve(data, num_trials, train_percentages)
+	c = 10**-1
+	cross_validation(digits)
